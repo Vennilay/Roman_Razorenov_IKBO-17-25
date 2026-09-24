ab18a187348c9cce4a78ea12c85925b586c6ae97
Vennilay <229026665+Vennilay@users.noreply.github.com>
2026-09-24 16:24:19 +0300

    add 1.md

--- PR_1/task6.sh
+++ PR_1/task6.sh
@@ -1,7 +1,7 @@
-    #!/bin/bash
+#!/bin/bash
 
-    for file in files/*.{c,js,py}; do
-        if [ -f "$file" ]; then
-            head -n 1 "$file" | grep -qE '^(//|/\*|#)' && echo "$file: Есть" || echo "$file: Нет"
-        fi
-    done
+for file in files/*.{c,js,py}; do
+    if [ -f "$file" ]; then
+        head -n 1 "$file" | grep -qE '^(//|/\*|#)' && echo "$file: Есть" || echo "$file: Нет"
+    fi
+done
